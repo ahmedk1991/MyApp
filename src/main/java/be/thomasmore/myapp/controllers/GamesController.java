fe05ae5b854907d28190ebd62a5c3dc2ccd9eaf2
ahmed <ahmed.kerkri@outlook.com>
2024-03-09 20:49:01 +0100

    form bijgemaakt om nieuwe spellen toe te voegen

--- src/main/java/be/thomasmore/myapp/controllers/GamesController.java
+++ src/main/java/be/thomasmore/myapp/controllers/GamesController.java
@@ -3,10 +3,14 @@ package be.thomasmore.myapp.controllers;
 import be.thomasmore.myapp.model.Games;
 import be.thomasmore.myapp.model.GamesDto;
 import be.thomasmore.myapp.repositories.GamesRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -32,7 +36,16 @@ public class GamesController {
         return "products/createproduct";
     }
     @PostMapping("/create")
-    public String createProduct()
-
-
+    public String createProduct(
+         @Valid @ModelAttribute GamesDto gamesDto,
+         BindingResult result
+    ) {
+    if(gamesDto.getImageFile().isEmpty()){
+        result.addError(new FieldError("gamesDto","imageFile","The image file is required"));
+    }
+    if(result.hasErrors()){
+        return "products/createproduct";
+    }
+    return "redirect:/products";
+    }
 }
