4830115778d4fcf9479089870425e9c1d60bbc7a
ahmed <ahmed.kerkri@outlook.com>
2024-04-05 06:22:35 +0200

    contact form bijgemaakt bevat nog errors

--- src/main/java/be/thomasmore/myapp/email/ContactController.java
+++ src/main/java/be/thomasmore/myapp/email/ContactController.java
@@ -3,7 +3,7 @@ package be.thomasmore.myapp.email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
